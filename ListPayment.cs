d5250d711b46ea22ebcfd2b12cb47f5489cefd75
William <capuletos@live.com>
2021-05-15 23:19:01 -0300

    first commit

---
+++ ListPayment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsTest
+{
+    class ListPayment
+    {
+        public int Id { get; set; }
+        public ListPayment(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            IEnumerable<ListPayment> payments = new List<ListPayment>();
+
+            payments.ToList(); //Faz a conversão para lista.
+        
+
+        }
+    }
+}
